3dc8f270e1fbd79d491f7cc61d8decac59854354
Igor Kazakov <i.s.kazakov89@gmail.com>
2023-08-31 11:20:21 +0300

    add magic methods, homework 14.1

--- src/channel.py
+++ src/channel.py
@@ -21,10 +21,42 @@ class Channel:
             self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute())
         self.title = self.channel_dict['items'][0]['snippet']['title']
         self.description = self.channel_dict['items'][0]['snippet']['description']
-        self.url = self.channel_dict['items'][0]['snippet']['thumbnails']['default']['url']
+        self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
         self.subscriberCount = self.channel_dict['items'][0]['statistics']['subscriberCount']
         self.video_count = self.channel_dict['items'][0]['statistics']['videoCount']
         self.view_count = self.channel_dict['items'][0]['statistics']['viewCount']
+
+    def __str__(self):
+        """отображение информации об объекте класса для пользователей"""
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        """сложение количества подписчиков"""
+        return int(self.subscriberCount) + int(other.subscriberCount)
+
+    def __sub__(self, other):
+        """вычитание количества подписчиков"""
+        return int(self.subscriberCount) - int(other.subscriberCount)
+
+    def __lt__(self, other):
+        """сравнение (меньше) количества подписчиков"""
+        return int(self.subscriberCount) < int(other.subscriberCount)
+
+    def __le__(self, other):
+        """сравнение (меньше или равно) количества подписчиков"""
+        return int(self.subscriberCount) <= int(other.subscriberCount)
+
+    def __gt__(self, other):
+        """сравнение (больше) количества подписчиков"""
+        return int(self.subscriberCount) > int(other.subscriberCount)
+
+    def __ge__(self, other):
+        """сравнение (больше или равно) количества подписчиков"""
+        return int(self.subscriberCount) >= int(other.subscriberCount)
+
+    def __eq__(self, other):
+        """сравнение (равно) количества подписчиков"""
+        return int(self.subscriberCount) == int(other.subscriberCount)
 
     @property
     def get_channel_id(self):
